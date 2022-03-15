6cac629446735f03123c9788bb0005c0be680fac
Geraud Hossou <hossougeraud5@gmail.com>
2022-03-15 23:16:06 +0100

    hello alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
 */                                                                                                                             
 int main(void)                                                                                                                
 {                                                                                                                               
-_putchar('-');                                                                                                                  
+_putchar('_');                                                                                                                  
 _putchar('p');                                                                                                                  
 _putchar('u');                                                                                                                  
 _putchar('t');                                                                                                                  
@@ -13,6 +13,6 @@ _putchar('c');
 _putchar('h');                                                                                                                  
 _putchar('a');                                                                                                                  
 _putchar('r');                                                                                                                  
-_putchar('\n');                                                                                                                 
+_putchar(' \n ');                                                                                                                 
 return (0);                                                                                                                     
 }
