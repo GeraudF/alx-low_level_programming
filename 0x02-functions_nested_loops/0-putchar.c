b5c66561ef5d9a52d32bcd15911db241b064477c
Geraud Hossou <hossougeraud5@gmail.com>
2022-03-15 19:51:29 +0100

    hello alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
-#include "main.h"
-/**
-* main - Print "_putchar" followed by a new line.
-* Description: You are not allowed to include standard libraries.
-* Return: 0
-*/
-int main(void)
-{
-char _putchar[] = "_putchar";
-int i = 0;
-
-while (_putchar[i] != '\0')
-{
-_putchar(_putchar[i]);
-i++;
-}
-_putchar('\n');
-
-return (0);
+#include "main.h"                                                                                                               
+/**                                                                                                                             
+*main -  prints putchar followed by a new line                                                                                 
+*Return: Always 0 (Success)                                                                                                    
+*/                                                                                                                             
+int main(void)                                                                                                                
+{                                                                                                                               
+_putchar('_');                                                                                                                  
+_putchar('p');                                                                                                                  
+_putchar('u');                                                                                                                  
+_putchar('t');                                                                                                                  
+_putchar('c');                                                                                                                  
+_putchar('h');                                                                                                                  
+_putchar('a');                                                                                                                  
+_putchar('r');                                                                                                                  
+_putchar('\n');                                                                                                                 
+return (0);                                                                                                                     
 }
