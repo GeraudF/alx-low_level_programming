d04a9269e449191bcf72669218bb56fe8fb97fc9
Geraud Hossou <hossougeraud5@gmail.com>
2022-03-23 20:51:08 +0100

    hello

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,24 +2,22 @@
 
 /**
  * _strncat - concatenate two strings
- * @dest: char string to concatenate to
- * @src: char string
- * Return: pointer to resulting string `dest`
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
  */
 
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 int i, c;
 
 for (i = 0; dest[i] != '\0'; i++)
 ;
 
-for (c = 0; src[c] != '\0'; c++)
+for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 {
 dest[i] = src[c];
-i++;
 }
-
-dest[i] = '\0';
 return (dest);
 }
